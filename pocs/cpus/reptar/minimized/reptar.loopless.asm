d4b95f3f0ffd10d36a17bf694b36f69c5cb94f78
eduardo <sirdarckcat@gmail.com>
2023-11-20 19:52:53 +0100

    use sched_deadline

--- pocs/cpus/reptar/minimized/reptar.loopless.asm
+++ pocs/cpus/reptar/minimized/reptar.loopless.asm
@@ -57,10 +57,38 @@ global _start
             nop
 %endmacro
 
+section .bss
+    struc sched_attr 
+        size:             resd 1  ; Size of this structure
+        sched_policy:     resd 1  ; Policy (SCHED_*)
+        sched_flags:      resq 1  ; Flags
+        sched_nice:       resd 1  ; Nice value (SCHED_OTHER, SCHED_BATCH)
+        sched_priority:   resd 1  ; Static priority (SCHED_FIFO, SCHED_RR)
+        sched_runtime:    resq 1  ;
+        sched_deadline:   resq 1  ;
+        sched_period:     resq 1  ;
+        sched_util_min:   resd 1  ;
+        sched_util_max:   resd 1  ;
+    endstruc
+
 section .data
     one: dq 0x1
     magic: dq 0xcccccccc
     data: times 512 db 0
+
+    deadline_scheduled_attr: 
+        istruc sched_attr
+            at size, dd sched_attr_size ; SCHED_ATTR_SIZE_VER0
+            at sched_policy, dd 6       ; SCHED_DEADLINE=6
+            at sched_flags, dq 0        ; no flags
+            at sched_nice, dd 0         ; nice (ignored?)
+            at sched_priority, dd 0     ; priority (ignored)
+            at sched_runtime,  dq 1000*1000 ; 
+            at sched_deadline, dq 1000*1000 ; 
+            at sched_period,   dq 1000*1000 ; same as deadline
+            at sched_util_min, dd 0
+            at sched_util_max, dd 0
+        iend
 
 section .text
     _start:
@@ -71,6 +99,13 @@ section .text
         clflush [data]
         clflush [one]
         clflush [magic]
+        mov rax, 314 ; sched_setattr
+        mov rdi, 0   ; pid=0 for current
+        mov rsi, deadline_scheduled_attr ; attr
+        mov rdx, 0 ; flags
+        syscall
+        mov rax, 24 ; sched_yield
+        syscall
         %rep 2
             loopless_reptar
             inc rsp
