06d8a6a13a16336fc7c503e7989a34fd66c8ae48
eduardo <sirdarckcat@gmail.com>
2023-11-28 21:01:39 +0100

    add mce logger

---
+++ pocs/cpus/reptar/minimized/log_mce.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -ex
+
+cat reptar.log
+
+sudo mount -t debugfs none /sys/kernel/debug
+echo 1 | sudo tee /sys/kernel/debug/mce/fake_panic
+echo 0 | sudo tee /proc/sys/kernel/watchdog
+echo 0 | sudo tee /proc/sys/kernel/printk_ratelimit
+echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
+echo 0 | sudo tee /sys/bus/cpu/devices/cpu15/online
+
+touch reptar.mce.asm
+make reptar.mce.out || true
+
+for i in {1..10}; do
+    echo $i | tee reptar.log
+    sudo sync
+    sleep 0.3s
+    taskset -c 7 ./reptar.mce.out || true
+    sleep 1s
+    sudo cat /sys/kernel/debug/mce/severities-coverage | tr '\n' , | tr '\t' :
+    sudo dmesg | grep mce:
+done
