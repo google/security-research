7614918bd314641b6701d19675be205435b80456
Tamas Koczka <poprdi@google.com>
2024-01-12 13:13:48 +0000

    kernelCTF: simulator: switch to rootfs_v3 image

--- kernelctf/simulator/local_runner.sh
+++ kernelctf/simulator/local_runner.sh
@@ -21,20 +21,20 @@ set -- "${ARGS[@]}"
 RELEASE_NAME="$1"
 if [ -z "$RELEASE_NAME" ]; then usage; fi
 
-if [ ! -f "qemu_v2.sh" ]; then wget https://storage.googleapis.com/kernelctf-build/files/qemu_v2.sh; fi
-chmod u+x qemu_v2.sh
+if [ ! -f "qemu_v3.sh" ]; then wget https://storage.googleapis.com/kernelctf-build/files/qemu_v3.sh; fi
+chmod u+x qemu_v3.sh
 
 if [ ! -d "releases/$RELEASE_NAME" ]; then mkdir -p "releases/$RELEASE_NAME"; fi
 if [ ! -f "releases/$RELEASE_NAME/bzImage" ]; then
     wget -O "releases/$RELEASE_NAME/bzImage" "https://storage.googleapis.com/kernelctf-build/releases/$RELEASE_NAME/bzImage"
 fi
 
-if [ ! -f "rootfs_v1.img" ]; then
-    wget https://storage.googleapis.com/kernelctf-build/files/rootfs_v1.img.gz
-    gzip -d rootfs_v1.img.gz
+if [ ! -f "rootfs_v3.img" ]; then
+    wget https://storage.googleapis.com/kernelctf-build/files/rootfs_v3.img.gz
+    gzip -d rootfs_v3.img.gz
 fi
 
 if [ ! -f "ramdisk_v1.img" ]; then wget https://storage.googleapis.com/kernelctf-build/files/ramdisk_v1.img; fi
 if [ ! -f "flag" ]; then echo "kernelCTF{example_flag}" > flag; fi
 
-exec ./qemu_v2.sh "releases/$RELEASE_NAME" flag "$INIT_FN"
+exec ./qemu_v3.sh "releases/$RELEASE_NAME" flag "$INIT_FN"
